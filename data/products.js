fead2044fde56d205f78209f995a029c4fa1b029
Sunil Ahirwar <ahirwarsunil25@gmail.com>
2024-12-14 15:50:33 +0530

    update index

---
+++ data/products.js
@@ -0,0 +1,45 @@
+const products = [{
+    image: "image/lenovo-ideaPad-laptop.jpg",
+    name:'Lenovo ideapad Laptop | 16 GB RAM | i5 |SSD 256GB',
+    rating:{
+        stars:4.5,
+        count:87
+    },
+    pricePaisa:3399000,
+    mrp:5000000
+},
+
+{
+image:'image/banq-24inch-monitor.jpg',
+name:'Banq 24 Inch monitor | IPS display refresh rate 120 hrz FHD',
+rating:{
+    stars:3.5,
+    count:127
+},
+pricePaisa:999000,
+mrp:1500000
+},
+
+{
+image:'image/power-cable.jpg',
+name:'Frontech Power cable | three pin | Frequency 50hrz',
+rating:{
+    stars:4.5,
+    count:56
+},
+pricePaisa:39000,
+mrp:50000
+},
+
+{
+    image:'image/safari-pentagon.jpg',
+    name:'Safari pentagon Luxury traly bag | 90cm',
+    rating:{
+        stars:4,
+        count:2197
+    },
+    pricePaisa:190000,
+    mrp:300000
+},{
+    
+}];
